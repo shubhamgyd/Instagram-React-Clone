f993cd5bc00c5e2e6fa75ba6853c5159238de521
shubhamyadav97660@gmail.com <shubhamyadav97660@gmail.com>
2023-12-28 13:57:04 +0530

    eighth

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,4 +1,7 @@
 import { Container, Flex } from "@chakra-ui/react";
+import ProfileHeader from "../../components/Profile/ProfileHeader";
+import ProfileTabs from "../../components/Profile/ProfileTabs";
+import ProfilePosts from "../../components/Profile/ProfilePosts";
 
 const ProfilePage = () => {
   return <Container maxW="container.lg" py={5}>
@@ -12,8 +15,18 @@ const ProfilePage = () => {
     >
       <ProfileHeader />
     </Flex>
-    <Flex>
+    <Flex
+      px={{base: 2, sm:4}}
+      maxW={"full"}
+      mx={"auto"}
+      borderTop={"1px solid"}
+      borderColor={"whiteAlpha.300"}
+      direction={"column"}
+    >
+
       <ProfileTabs />
+      <ProfilePosts />
+
     </Flex>
   </Container>;
 };
