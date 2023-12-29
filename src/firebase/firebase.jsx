3f2065cc96b5783a6352068fca642570e7dadcb1
shubhamyadav97660@gmail.com <shubhamyadav97660@gmail.com>
2023-12-29 23:21:05 +0530

    firebase set-up is done

---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFireStore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSdzH6UfZN3PttFOgOhqslvW83rE3iFNg",
+  authDomain: "insta-clone-yt-b0604.firebaseapp.com",
+  projectId: "insta-clone-yt-b0604",
+  storageBucket: "insta-clone-yt-b0604.appspot.com",
+  messagingSenderId: "1003808727170",
+  appId: "1:1003808727170:web:cd0565bf9e86575f74149b",
+  measurementId: "G-YHT92BW017",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const fireStore = getFireStore(app);
+const storage = getStorage(app);
+
+export { app, auth, fireStore, storage };
