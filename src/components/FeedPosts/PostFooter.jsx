b7e41b4eb419ba34fbe019a4a3b07f5091a58650
shubhamyadav97660@gmail.com <shubhamyadav97660@gmail.com>
2023-12-29 23:04:35 +0530

    npm install firebase zustand react-firebase-hooks @chakra-ui/icons

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,8 +1,20 @@
-import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text } from "@chakra-ui/react";
-import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assests/constants";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import {
+  CommentLogo,
+  NotificationsLogo,
+  UnlikeLogo,
+} from "../../assests/constants";
 import { useState } from "react";
 
-const PostFooter = ({username}) => {
+const PostFooter = ({ username, isProfilePage }) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(1000);
 
@@ -16,7 +28,7 @@ const PostFooter = ({username}) => {
     }
   };
   return (
-    <Box mb={10}>
+    <Box mb={10} marginTop={"auto"}>
       <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={2}>
         <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
           {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
@@ -26,18 +38,22 @@ const PostFooter = ({username}) => {
           <CommentLogo />
         </Box>
       </Flex>
-      <Text fontWeight={600} color={'gray'}>
+      <Text fontWeight={600} color={"gray"}>
         {likes} likes
       </Text>
-      <Text fontSize="sm" fontWeight={700}>
-        {username}{" "}
-        <Text as='span' fontWeight={400}>
-          Feeling good
-        </Text>
-      </Text>
-      <Text fontSize='sm' color={'gray'}>
-        View all 1,000 comments
-      </Text>
+      {!isProfilePage && (
+        <>
+          <Text fontSize="sm" fontWeight={700}>
+            {username}{" "}
+            <Text as="span" fontWeight={400}>
+              Feeling good
+            </Text>
+          </Text>
+          <Text fontSize="sm" color={"gray"}>
+            View all 1,000 comments
+          </Text>
+        </>
+      )}
 
       <Flex
         alignItems={"center"}
@@ -46,19 +62,23 @@ const PostFooter = ({username}) => {
         w={"full"}
       >
         <InputGroup>
-          <Input variant={"flushed"} placeholder={"Add a comment..."} fontSize={14}/>
-            <InputRightElement>
-              <Button
-                fontSize={14}
-                color={"blue.500"}
-                fontWeight={600}
-                cursor={"pointer"}
-                _hover={{color: "white"}}
-                bg={"transparent"}
-              >
-                Post
-              </Button>
-            </InputRightElement>
+          <Input
+            variant={"flushed"}
+            placeholder={"Add a comment..."}
+            fontSize={14}
+          />
+          <InputRightElement>
+            <Button
+              fontSize={14}
+              color={"blue.500"}
+              fontWeight={600}
+              cursor={"pointer"}
+              _hover={{ color: "white" }}
+              bg={"transparent"}
+            >
+              Post
+            </Button>
+          </InputRightElement>
         </InputGroup>
       </Flex>
     </Box>
