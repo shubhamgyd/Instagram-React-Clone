5738ecfbafa331e8fcd981f39316ff6b5ce38dac
shubhamyadav97660@gmail.com <shubhamyadav97660@gmail.com>
2023-12-29 17:05:48 +0530

    Profile Post created

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,9 +1,42 @@
-import { Grid } from '@chakra-ui/react'
+import { Grid, GridItem, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { AiFillHeart } from 'react-icons/ai'
 
-const ProfilePost = () => {
+const ProfilePost = ({img}) => {
   return (
-    <>Profile Post</>
+    <GridItem
+      cursor={"pointer"}
+      borderRadius={4}
+      overflow={"hidden"}
+      border={"1px solid"}
+      borderColor={"whiteApha.300"}
+      aspectRatio={1/1}
+    >
+      <Flex 
+        opacity={0}
+        _hover={{opacity:1}}
+        position={"absolute"}
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        bg={"blackAlpha.700"}
+        transition={"all 0.3s ease"}
+        zIndex={1}
+        justifyContent={"center"}
+      >
+        <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+          <Flex>
+            <AiFillHeart size={20}/>
+            <Text fontWeight={"bold"} ml={2}>
+              7
+            </Text>
+          </Flex>
+          <Flex></Flex>
+        </Flex>
+      <Image src={img}></Image>
+      </Flex>
+    </GridItem>
   )
 }
 
--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,33 +1,44 @@
-import { Box, Skeleton, VStack, Grid } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
+import { Box, Skeleton, VStack, Grid } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import ProfilePost from "./ProfilePost";
 
 const ProfilePosts = () => {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-      setIsLoading(false)
-    },2000)
-  },[])
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <Grid
       templateColumns={{
-        sm:"repeat(1,1fr)",
-        md:"repeat(3,1fr)",
+        sm: "repeat(1,1fr)",
+        md: "repeat(3,1fr)",
       }}
       gap={1}
       columnGap={1}
     >
-      {true && [0,1,2,4].map((_,idx)=>(
-        <VStack key={idx} alignItems={"flex-start"}>
-          <Skeleton w={"full"}>
-            <Box h="300px">contents wrapped</Box>
-          </Skeleton>
-        </VStack>
-      ))}
-    </Grid>
-  )
-}
+      {isLoading &&
+        [0, 1, 2, 3, 4, 5].map((_, idx) => (
+          <VStack key={idx} alignItems={"flex-start"}>
+            <Skeleton w={"full"}>
+              <Box h="300px">contents wrapped</Box>
+            </Skeleton>
+          </VStack>
+        ))}
 
-export default ProfilePosts
+      {!isLoading && (
+        <>
+          <ProfilePost img="/img1.png" />
+          <ProfilePost img="/img2.png" />
+          <ProfilePost img="/img3.png" />
+          <ProfilePost img="/img4.png" />
+        </>
+      )}
+    </Grid>
+  );
+};
+
+export default ProfilePosts;
