5f1c47982a4fb1910ea6cb923da5c04ff80a64b8
shubhamyadav97660@gmail.com <shubhamyadav97660@gmail.com>
2024-01-09 13:09:31 +0530

    FeedPosts completed

--- src/components/FeedPosts/FeedPost.jsx
+++ src/components/FeedPosts/FeedPost.jsx
@@ -1,15 +1,17 @@
 import {Box, Image} from '@chakra-ui/react';
 import PostHeader from "./PostHeader";
 import PostFooter from './PostFooter';
+import useGetUserProfileById from "../../hooks/useGetUserProfileById"
 
-const FeedPost = ({img,username,avatar}) => {
+const FeedPost = ({post}) => {
+  const {userProfile} = useGetUserProfileById(post.createdBy)
   return (
     <>
-      <PostHeader username={username} avatar={avatar}/>
+      <PostHeader post={post} creatorProfile={userProfile}/>
       <Box my={2} borderRadius={4} overflow={"hidden"}>
-        <Image src={img} alt={username} />
+        <Image src={post.imageURL} alt={"Feed Post Image"} />
       </Box>
-      <PostFooter username={username} />
+      {/* <PostFooter username={username} /> */}
     </>
   )
 }
--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -1,22 +1,22 @@
-import { Container, Flex, Box, Skeleton, SkeletonCircle, VStack } from "@chakra-ui/react";
+import {
+  Container,
+  Flex,
+  Box,
+  Skeleton,
+  SkeletonCircle,
+  VStack,
+} from "@chakra-ui/react";
 import FeedPost from "./FeedPost";
-import { useState } from "react";
-import { useEffect } from "react";
+import useGetFeedPosts from "../../hooks/useGetFeedPosts";
 
 const FeedPosts = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(()=>{
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
+  const { isLoading, posts } = useGetFeedPosts();
   return (
     <Container maxW={"container.sm"} py={10} px={2}>
-      {isLoading && 
-        [0,1,2,3].map((_,idx) => (
+      {isLoading &&
+        [0, 1, 2, 3].map((_, idx) => (
           <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
-            <Flex gap='2'>
+            <Flex gap="2">
               <SkeletonCircle size={10} />
               <VStack gap={2} alignItems={"flex-start"}>
                 <Skeleton height={10} w={200} />
@@ -24,27 +24,12 @@ const FeedPosts = () => {
               </VStack>
             </Flex>
             <Skeleton w={"full"}>
-              <Box h={'500px'} />
+              <Box h={"500px"} />
             </Skeleton>
           </VStack>
-  ))}
+        ))}
 
-    {!isLoading && (
-      <>
-      <FeedPost img="/img1.png" username="burakorkmezz" avatar="/img1.png">
-        FeedPost
-      </FeedPost>
-      <FeedPost img="/img2.png" username="josh" avatar="/img2.png">
-        FeedPost
-      </FeedPost>
-      <FeedPost img="/img3.png" username="janedoe" avatar="/img3.png">
-        FeedPost
-      </FeedPost>
-      <FeedPost img="/img4.png" username="johndoe" avatar="/img4.png">
-        FeedPost
-      </FeedPost>
-      </>
-    )}
+      {!isLoading && posts.length > 0 && posts.map((post) => <FeedPost key={post.id} post={post}/>)}
     </Container>
   );
 };
